40a17ad58d4c0562c1253e9ad51d118c50b0155a
Nihal <nihal@warewe.com>
2025-12-23 17:29:43 +0530

    Platform section logo cloud done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CTA from "@/components/CTA/CTA";
 import Hero from "@/components/Hero/Hero";
 import LatestTools from "@/components/LatestTools/LatestTools";
+import LogoCloud from "@/components/LogoCloud";
 import TopAITools from "@/components/TopAITools/TopAITools";
 import TrendingTopics from "@/components/TrendingTopics";
 
@@ -13,6 +14,7 @@ export default function Home() {
       <TopAITools />
       <CTA />
       <TrendingTopics />
+      <LogoCloud />
     </main>
   );
 }
---
+++ components/LogoCloud.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+const brands = [
+    { name: "Meta", logo: "/platforms/meta.png" },
+    { name: "Microsoft", logo: "/platforms/microsoft.png" },
+    { name: "Stability AI", logo: "/platforms/stabilityai.png" },
+    { name: "DeepMind", logo: "/platforms/deepmind.png" },
+    { name: "Google", logo: "/platforms/google.png" },
+    { name: "OpenAI", logo: "/platforms/openai.png" },
+];
+
+export default function LogoCloud() {
+    return (
+        <section className="bg-slate-950 text-white px-4 sm:px-6 lg:px-10 py-20 overflow-hidden">
+            <div className=" mx-auto">
+                <h2 className="mb-12 text-center text-3xl sm:text-4xl font-semibold
+                    bg-linear-to-r from-amber-400 to-purple-600
+                    bg-clip-text text-transparent">
+                    Popular Platforms
+                </h2>
+
+                <div className="relative w-full overflow-hidden mt-20
+                 mask-[linear-gradient(to_right,transparent_0,white_128px,white_calc(100%-128px),transparent_100%)]">
+                    <div className="flex w-max animate-logo-scroll gap-16">
+                        {[...brands, ...brands].map(({ name, logo }, i) => (
+                            <div
+                                key={name + i}
+                                className="flex items-center justify-center min-w-35"
+                            >
+                                <Image
+                                    src={logo}
+                                    alt={name}
+                                    width={160}
+                                    height={80}
+                                    className="h-10 w-auto object-contain opacity-80 hover:opacity-100 transition"
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
